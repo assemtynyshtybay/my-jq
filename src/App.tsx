959b5efddcdce75ea9c4052bd74f069e866d7e7a
Altynai <talipaltynai00@gmail.com>
2022-06-28 14:54:08 +0600

    search-panel

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Navbar } from './components/Navbar';
+import { Search } from './components/search-panel/Search';
 
 function App() {
   return (
     <div className="App">
-       <Navbar />
        
+       <Navbar />
+
+       <Search />
+
     </div>
   );
 }
