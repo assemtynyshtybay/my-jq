41088622f06242948c044337ec7d7c33f4d9a532
assemtynyshtybay <asem201000@gmail.com>
2022-07-02 16:19:09 +0600

    add similar jobs

--- src/components/job/JobDetail.tsx
+++ src/components/job/JobDetail.tsx
@@ -3,6 +3,8 @@ import { Button, Container, styled } from '@mui/material';
 import { FC } from 'react';
 import { Job } from '../types/jobsTypes';
 import { useNavigate, useParams } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { fetchSimilarJobs } from '../store/actions/jobActionCreator';
 type Props = {
   job: Job;
 };
@@ -15,12 +17,32 @@ const Box = styled('div')`
   font-size: 26px;
   /* background: linear-gradient(180deg, rgba(29, 29, 29, 0) 0%, rgba(29, 29, 29, 0.8) 80.79%); */
 `;
+const SimilarJob = styled('button')`
+  width: 55%;
+  display: flex;
+  flex-direction: column;
+  margin: 10px;
+  padding: 20px;
+  border: 1px solid gray;
+  border-radius: 10px;
+  font-size: 26px;
+  text-align: left;
+  /* background: linear-gradient(180deg, rgba(150, 29, 29, 0) 0%, rgba(29, 29, 29, 0.8) 99.79%); */
+`;
+const SimilarJobs = styled('div')`
+  display: flex;
+  margin: 0 auto;
+  width: 55%;
+  overflow-x: auto;
+`;
+
 const Description = styled('div')``;
 const Salary = styled('div')``;
 const Title = styled('div')``;
 const Time = styled('div')``;
 const JobDetails = () => {
   const params = useParams();
+  const dispatch = useDispatch();
   const [job, setData] = useState<Job>();
   const [similarJobs, setSimilarJobs] = useState<Job[]>();
   const navigate = useNavigate();
@@ -28,7 +50,16 @@ const JobDetails = () => {
     fetch(`https://api.hh.ru/vacancies/${params.id}`)
       .then((res) => res.json())
       .then((data) => setData(data));
+    fetch(`https://api.hh.ru/vacancies/${params.id}/similar_vacancies`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log('similar', data);
+        setSimilarJobs(data.items);
+      });
   }, [params.id]);
+
   console.log('job', job);
   return (
     <>
@@ -67,6 +98,19 @@ const JobDetails = () => {
           </Button>
         </Box>
       )}
+      <hr />
+      <SimilarJobs>
+        {similarJobs?.map((job) => (
+          <SimilarJob onClick={() => navigate(`/job-details/${+job.id}`)}>
+            <Title>{job.name}</Title>
+            <Time>{job.published_at}</Time>
+            <Description>{job.schedule.name}</Description>
+            <Salary>
+              {job.salary?.from}тг до {job.salary?.to}тг{' '}
+            </Salary>
+          </SimilarJob>
+        ))}
+      </SimilarJobs>
     </>
   );
 };
--- src/components/store/actions/jobActionCreator.ts
+++ src/components/store/actions/jobActionCreator.ts
@@ -1,8 +1,7 @@
 import { Dispatch } from 'react';
 import { JobActionType } from '../../types/jobsTypes';
  
-export const fetchJobs = 
-({page = 0}={}) =>(dispatch: Dispatch<any>) => {
+export const fetchJobs = ({page = 0}={}) =>(dispatch: Dispatch<any>) => {
     fetch(`https://api.hh.ru/vacancies`).then((res) => {
         return res.json();
     }).then(data => {
@@ -12,6 +11,13 @@ export const fetchJobs =
     })
 };
 
-export const
+export const fetchSimilarJobs = ({id = 1}) => (dispatch: Dispatch<any>) => {
+    fetch(`https://api.hh.ru/vacancies/${id}/similar_vacancies`).then((res) => {
+        return res.json();
+    }).then(data => {
+        console.log('similar', data)
+        dispatch({type: JobActionType.FETCH_JOBS, payload: data.items});
+    })
+}
 
 
