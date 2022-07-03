e5a05c430de3bfc60941a9eba59af2e4333c1c2f
assemtynyshtybay <asem201000@gmail.com>
2022-07-04 02:09:15 +0600

    fix style

--- src/components/job/JobDetail.tsx
+++ src/components/job/JobDetail.tsx
@@ -3,9 +3,9 @@ import { Button, styled } from '@mui/material';
 import { Job } from '../types/jobsTypes';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
+import moment from 'moment';
 import Loc from '../../assets/Lock.svg';
-import icons from '../../assets/icons.png'
-
+import icons from '../../assets/icons.png';
 
 const Box = styled('div')`
   width: 50%;
@@ -13,15 +13,18 @@ const Box = styled('div')`
   flex-direction: column;
   margin: 0 auto;
   padding: 50px;
- font-size: 45px;
- position: relative;
+  font-size: 45px;
+  position: relative;
 
   /* background: linear-gradient(180deg, rgba(29, 29, 29, 0) 0%, rgba(29, 29, 29, 0.8) 80.79%); */
 `;
 const SimilarJob = styled('button')`
-  width: 55%;
   display: flex;
+  flex-basis: 500px;
+  flex-grow: 0;
+  flex-shrink: 0;
   flex-direction: column;
+  justify-content: space-evenly;
   margin: 10px;
   padding: 20px;
   border: 1px solid gray;
@@ -34,10 +37,10 @@ const SimilarJob = styled('button')`
 const SimilarJobs = styled('div')`
   display: flex;
   margin: 0 auto;
+  padding: 48px 0px;
   width: 55%;
   overflow-x: auto;
-  
-
+  background: linear-gradient(180deg, #9D355D 0%, rgba(29, 29, 29, 0.8) 99.79%);
 `;
 const Title = styled('div')`
   font-weight: bold;
@@ -46,44 +49,44 @@ const Title = styled('div')`
   /* font-weight: 400; */
   font-style: normal;
   line-height: 42px;
+  height: 100%;
+  overflow: hidden;
 `;
 
 const Description = styled('div')`
-
+  font-size: 24px;
+  margin-top: 10px;
 `;
-const Salary = styled('div')``;
+const Salary = styled('div')`
+  font-size: 24px;
+  margin-top: 10px;
+`;
 
 const Time = styled('div')`
-font-size: 32px;
-margin: 20px;
-color: #585858;
+  font-size: 32px;
+  margin: 20px;
+  color: #585858;
 `;
 
 const Text = styled('div')`
-text-align: center;
-font-size: 38px;
+  text-align: center;
+  font-size: 38px;
 `;
 
-const Location =styled("div")`
-
-`
-const Img=styled('img')`
+const Location = styled('div')``;
+const Img = styled('img')`
   height: 5vmin;
   /* display: flex; */
-
- 
-
-`
-const ImgLike=styled('img')`
-   height: 5vmin; 
-   
-  ;
-`
+`;
+const ImgLike = styled('img')`
+  height: 5vmin; ;
+`;
 
 const JobDetails = () => {
   const params = useParams();
   const dispatch = useDispatch();
   const [job, setData] = useState<Job>();
+
   const [similarJobs, setSimilarJobs] = useState<Job[]>();
   const navigate = useNavigate();
   useEffect(() => {
@@ -104,22 +107,29 @@ const JobDetails = () => {
     <>
       {job && (
         <Box>
-
-          <Title>  
-          {job.name} / {job.schedule.name}<Button><ImgLike src={icons} /></Button>
+          <Title>
+            {job.name} / {job.schedule.name}
+            <Button>
+              <ImgLike src={icons} />
+            </Button>
           </Title>
 
-          <Time>Вакансия опубликована: {job.published_at}</Time>
-          
-          <Location><Img src={Loc} className="Loc-logo" alt="l-logo" /> {job.area.name}</Location>
-    
-          <Salary>
+          <Time>
+            Вакансия опубликована:&nbsp;{moment(job.published_at).format('hh:mm DD/MM/YYYY')}
+          </Time>
 
-            Зарплата:
+          <Location>
+            <Img src={Loc} className="Loc-logo" alt="l-logo" /> {job.area.name}
+          </Location>
+
+          <Salary>
+            Зарплата:&nbsp;
             {job.salary ? '' : 'Не указано'}
             {job.salary?.from} {job.salary?.from ? 'KZT' : null} {job.salary?.to ? 'до' : null}{' '}
             {job.salary?.to} {job.salary?.to ? 'KZT' : null}
           </Salary>
+          <Description>Описание:</Description>
+          <Description>{job.snippet?.requirement}</Description>
           <Description>{job.snippet?.responsibility}</Description>
 
           <Button
@@ -154,9 +164,11 @@ const JobDetails = () => {
 
       <SimilarJobs>
         {similarJobs?.map((job) => (
-          <SimilarJob onClick={() => navigate(`/job-details/${+job.id}`)}>
+          <SimilarJob
+            key={job.id}
+            style={{ backgroundColor: '#F8D198' }}
+            onClick={() => navigate(`/job-details/${+job.id}`)}>
             <Title>{job.name}</Title>
-            <Time>{job.published_at}</Time>
             <Description>{job.schedule.name}</Description>
             <Salary>
               Зарплата:
@@ -166,7 +178,7 @@ const JobDetails = () => {
             </Salary>
           </SimilarJob>
         ))}
-      </SimilarJobs> 
+      </SimilarJobs>
     </>
   );
 };
