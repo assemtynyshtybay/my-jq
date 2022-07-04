f805ce590037464f64428a3c9cb0c9b4b78bc239
assemtynyshtybay <asem201000@gmail.com>
2022-07-04 22:14:54 +0600

    Updates

--- src/components/hooks/useTypedSelector.ts
+++
@@ -1,4 +0,0 @@
-import { TypedUseSelectorHook, useSelector } from "react-redux";
-import { IState } from "../store/index";
-
-export const useTypedSelector: TypedUseSelectorHook<IState> = useSelector;
